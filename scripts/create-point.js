2efa69b8bce3d6baff183b5ffcde725650515b5d
Daniel Vieira <62304465+danielvieiralopes@users.noreply.github.com>
2020-06-05 00:49:59 -0300

    day 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -51,3 +51,57 @@ document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
 
+// Itens de coleta
+// pegar todos os li`s
+
+const itemsToCollect = document.querySelectorAll(".items-grid li") 
+
+for (const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]") 
+
+let selectItems = []
+
+function handleSelectedItem(event) {
+    
+    const itemLi = event.target
+
+    // adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+
+   
+    // verificar se existem itens selecionados, se sim 
+    // pegar os itens selecionados
+
+    const alreadySelected = selectItems.findIndex(item => {
+        const itemFound = item == itemId // isso sera true ou false
+        return itemFound
+    })
+
+    // se ja estiver selecionado,
+
+    if(alreadySelected >= 0){
+        // tirar da selecao
+        const filteredItems = selectItems.filter(item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferentse
+        })
+
+        selectItems = filteredItems
+    } else {
+        // se nao estiver selecionado,
+        //  add a selecao
+
+        selectItems.push(itemId)
+    }
+
+
+    //att o campo escondido com os itens selecionados
+    collectedItems.value = selectItems
+    
+
+}
