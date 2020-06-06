2f92ecbf8d5c081fe8ec70d2555b8069a5757e6d
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-06-06 14:11:22 -0300

    save-point

--- src/server.js
+++ src/server.js
@@ -5,22 +5,32 @@ const server = express()
 //Configura passta publica
 server.use(express.static("public"))
 
+
+//utilizando template engine 
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+
 // configurar caminhos da minha aplicação 
 
 //pagina inicial 
 //req: requisicao
 //res: respota
 server.get("/", (req, res) =>{
-     res.sendFile(__dirname + "/views/index.html")
+   return  res.render("index.html")
      
 })
 
 server.get("/create-point", (req, res) =>{
-    res.sendFile(__dirname + "/views/create-point.html")   
+    return res.render("create-point.html")   
 })
 
 server.get("/search-results", (req, res) =>{
-    res.sendFile(__dirname + "/views/search-results.html")   
+    return res.render("search-results.html")   
 })
 
 // ligar o servidor 
