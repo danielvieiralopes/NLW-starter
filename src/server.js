448cb5c29829f5cf0c95776d5d0f6b229f8e6b17
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-06-06 13:38:44 -0300

    criação do servidor

--- src/server.js
+++ src/server.js
@@ -1,1 +1,27 @@
-    
+const express = require("express")
+const server = express()
+
+
+//Configura passta publica
+server.use(express.static("public"))
+
+// configurar caminhos da minha aplicação 
+
+//pagina inicial 
+//req: requisicao
+//res: respota
+server.get("/", (req, res) =>{
+     res.sendFile(__dirname + "/views/index.html")
+     
+})
+
+server.get("/create-point", (req, res) =>{
+    res.sendFile(__dirname + "/views/create-point.html")   
+})
+
+server.get("/search-results", (req, res) =>{
+    res.sendFile(__dirname + "/views/search-results.html")   
+})
+
+// ligar o servidor 
+server.listen(3000)
